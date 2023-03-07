ac85421f864268ddd17a50e71e992af535fb4dc6
foxik <foxik@mail.ru>
2023-03-07 17:07:30 +0300

    lab05

---
+++ na3/Program.cs
@@ -0,0 +1,73 @@
+﻿Console.WriteLine("Формулы = 1; Циклы = 2");
+int vibor;
+vibor = Convert.ToInt32(Console.ReadLine());
+if (vibor == 1)
+{
+    Console.WriteLine("Выбран раздел: формулы");
+    Console.WriteLine("Геометрическая прогрессия");  // Геометрическая
+    Console.WriteLine("Введите первый член"); // a1
+    double b1 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите знаменатель  прогрессии"); // q
+    double q = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите номер последнего члена"); // n - число элементов
+    double n0 = Convert.ToDouble(Console.ReadLine());
+
+    double s = b1 * ((Math.Pow(q, n0)) - 1) / (q - 1);
+    Console.WriteLine($"Сумма геометрической прогрессии равна: {s}");
+
+    Console.WriteLine("Арифметическая прогрессия");
+    Console.WriteLine("Введите первый член");  // Арифметическая
+    double a1 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите разность");
+    double d = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите номер последнего члена"); // n - число элементов
+    double n = Convert.ToDouble(Console.ReadLine());
+
+    double s1 = (a1 + d) * n;
+
+    Console.WriteLine($"Сумма арифметической равна {s1}");
+}
+
+if (vibor == 2)
+
+{
+    Console.WriteLine("Выбран раздел: циклы");
+    Console.WriteLine("Геометрическая прогрессия"); ; // цикл геометрический
+    Console.WriteLine("Введите первый член"); // a1
+    double b11 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите знаменатель  прогрессии"); // q
+    double q1 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите номер последнего члена"); // n - число элементов
+    double n00 = Convert.ToDouble(Console.ReadLine());
+    double umn = b11;
+    for (double i = 0; i < (n00 - 1); i++)
+    {
+        b11 = b11 * q1;
+        umn = umn + b11;
+
+    }
+    Console.WriteLine(umn);
+
+    Console.WriteLine("Арифметическая прогрессия"); // цикл арифметической
+    Console.WriteLine("Введите первый член");
+    double a2 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите разность");
+    double d1 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите число элементов");
+    double n1 = Convert.ToDouble(Console.ReadLine());
+
+    double lastnumber = a2;
+    double sum = a2;
+
+
+    for (double i = 0; i < (n1 - 1); i++)
+    {
+        lastnumber = lastnumber + d1;
+        sum = sum + lastnumber;
+
+    }
+    Console.WriteLine(sum);
+
+
+}
+
---
+++ na4/ConsoleApp9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+Console.WriteLine("Бинарный поиск");
+Console.WriteLine("Введите минимальное значение диапазаона:");
+int i;
+i = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение диапазона:");
+int p;
+p = Convert.ToInt32(Console.ReadLine());
+int[] massiv = Enumerable.Range(i, p).ToArray();
+double low = 0;
+double high = massiv.Length - 1;
+double input = 0; // Входные данные пользователя
+int ser = 0; // середина
+Console.WriteLine($"Введите число от {i} до {p}");
+input = Convert.ToDouble(Console.ReadLine());
+
+
+while (massiv[ser] != input)
+{
+    ser = Convert.ToInt32(low + high / 2);
+
+    if (input < massiv[ser])
+    {
+        high = ser - 1;
+    }
+    else if (input > massiv[ser])
+    {
+        low = ser + 1;
+    }
+
+    Console.WriteLine($"Число - {input} Позиция в массиве - {ser}");
+
+
+
+
+}
